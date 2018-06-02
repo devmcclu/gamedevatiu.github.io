2bd123522b3a0c42a63426dd9e3d3750ea14703b
Rajin <rajshank>
2018-06-02 16:02:45 -0400

    Event info cards, started article system

--- js/remoteContent.js
+++ js/remoteContent.js
@@ -14,6 +14,10 @@ const listSeparator = ',';
 const listSuffix = "-list";
 const listItemFormat = "<div class='$key' tag='$value'></div>";
 
+const querySeparator = '?';
+const subquerySeparator = '#';
+const defaultQuery = 'none';
+
 function alertNotFound() {
     alert("404 File not found!");
 }
@@ -38,9 +42,9 @@ function getFileContents(filePath, K, failK) {
 
 // Parse raw xml into an object
 //  cleans newline characters and removes tabs
-function parse(content) {
+function parse(content, filePath) {
     result = {};
-    var inline = content.replace(/>(?:\r\n|\r|\n)/g, '>').replace(/(?:\r\n|\r|\n)</g, '<').replace(/,(?:\r\n|\r|\n|\s)/g, ',').replace(/\t/g, '');
+    var inline = content.replace(/>(?:\r\n|\r|\n)/g, '>').replace(/(?:\r\n|\r|\n)</g, '<').replace(/,(?:\r\n|\r|\n)/g, ',').replace(/\t/g, '');
     re = /<(.+?)>([\s\S]+?)<\/.+?>/g;
 
     var m = re.exec(inline);
@@ -50,6 +54,7 @@ function parse(content) {
         result[k] = v;
         m = re.exec(inline);
     }
+    result["filePath"] = filePath;
 
     return result;
 }
@@ -57,7 +62,6 @@ function parse(content) {
 // Fill target DOM element children with object content
 //  for each object attribute, fills first child with attribute name as class
 function inject(o, target) {
-    var html = "";
     for (var k in o) {
         let identifier = k.charAt(0);
         if (identifier == imageIdentifier) {
@@ -77,13 +81,15 @@ function inject(o, target) {
             }
         }
         else if (identifier == listIdentifier) {
-            let values = o[k].split(listSeparator);
+            let clean = o[k].replace(/,[\s+]/g, ",");
+            let values = clean.split(listSeparator);
 
             let key = k.slice(1);
             let listKey = key + listSuffix;
             let insert = target.getElementsByClassName(listKey);
 
             if (insert.length > 0) {
+                insert[0].innerHTML = "";
                 for (var i = 0; i < values.length; i++) {
                     let newItem = listItemFormat.replace(/\$key/g, key).replace(/\$value/g, values[i]);
                     insert[0].innerHTML += newItem;
@@ -97,6 +103,18 @@ function inject(o, target) {
             }
         }
     }
+
+    let injLinks = target.querySelectorAll('[injtarget]');
+    for (var i = 0; i < injLinks.length; i++) {
+        let e = injLinks[i];
+        let path = o["filePath"];
+        e.onclick = function () {
+            getFileContents(path, function (x) {
+                let target = document.getElementById(e.getAttribute("injtarget"));
+                inject(parse(x, path), target);
+            }, alertNotFound);
+        }
+    }
 }
 
 function injectMultipleFiles(filePaths, targets) {
@@ -107,10 +125,11 @@ function injectMultipleFiles(filePaths, targets) {
     let n = filePaths.length;
     for (var i = 0; i < n; i++) {
         let target = targets[i];
-        getFileContents(filePaths[i], function (x) {
-            console.log(parse(x));
-            console.log(target);
-            inject(parse(x), target);
+        let path = filePaths[i];
+        getFileContents(path, function (x) {
+            // console.log(parse(x));
+            // console.log(target);
+            inject(parse(x, path), target);
         }, alertNotFound);
     }
 }
@@ -121,7 +140,7 @@ function listRange(folder, start, end, target) {
         for (var i = 0; i < paths.length; i++) {
             paths[i] = contentDirectory + folder + "/" + paths[i] + fileExtenstion;
         }
-        console.log(paths);
+        // console.log(paths);
         var targets = [target];
         let num = Math.min(paths.length, end) - start;
         for (var i = 1; i < num; i++) {
@@ -130,5 +149,22 @@ function listRange(folder, start, end, target) {
             targets.push(newTarget);
         }
         injectMultipleFiles(paths.slice(start, end), targets);
+    }, alertNotFound);
+}
+
+function getQueryString() {
+    let fullURL = window.location.href;
+    let tokens = fullURL.split(querySeparator);
+    if (tokens.length < 2) {
+        return defaultQuery;
+    }
+    else return tokens[1].split(subquerySeparator)[0];
+}
+
+function injectFromQuery(folder, target) {
+    let q = getQueryString();
+    let path = contentDirectory + folder + "/" + q + fileExtenstion;
+    getFileContents(path, function (x) {
+        inject(parse(x, path), target);
     }, alertNotFound);
 }
