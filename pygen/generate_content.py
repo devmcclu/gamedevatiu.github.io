083dd9b6c07a6b8b1f31c09176e8bf2ab28c1a75
Rajin <rajin@rajinshankar.com>
2019-08-05 14:56:37 -0400

    Add comments to generate_content.py

--- pygen/generate_content.py
+++ pygen/generate_content.py
@@ -4,6 +4,8 @@ import sys
 
 from genutil import *
 
+# Read in info files based on a listing, generating pages according to a template
+# note: should be called before generating output HTML so the generated content pages can be processed as well
 def generate_content(template_path, content_folder):
     items = get_vars_listing(content_folder)
 
@@ -13,9 +15,13 @@ def generate_content(template_path, content_folder):
         print(f"Generate item {item_path} (out: {output_path})")
         generate_file_from_template(template_path, output_path, item)
 
+# Get the relevant arguments
 args = sys.argv[1:]
+
+# Check arguments
 if len(args) < 2:
     print("Usage: generate_content <template path> <content folder>")
 else:
+    # Do generation
     print(f"Generating items in '{args[1]}' with template '{args[0]}'")
     generate_content(args[0], args[1])
